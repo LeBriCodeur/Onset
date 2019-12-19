5444cb14043908c0267b0c12a9137c01c923bb2f
LeBriCodeur <52689896+LeBriCodeur@users.noreply.github.com>
2019-12-19 21:57:56 +0100

    V1 -EOP-

---
+++ MCF - Easy Onset Package/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MCF___Easy_Onset_Package
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(109, 213);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(167, 193);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nom du package";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.Silver;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(109, 242);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(245, 89);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(109, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Créer les packages séléctionné";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Silver;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(202, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MCF\r\nGames";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(287, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ajouter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Silver;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(204, 136);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Auteur";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Silver;
+            this.textBox2.Location = new System.Drawing.Point(146, 158);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(171, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(462, 418);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MCF - Easy Onset Package";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MCF - Easy Onset Package/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MCF___Easy_Onset_Package
+{
+    /*
+        public class PackageJson
+        {
+            public string author { get; set; }
+            public string version { get; set; }
+            public string[] server_scripts { get; set; }
+            public string[] client_scripts { get; set; }
+        }
+
+    */
+    public partial class Form1 : Form
+    {
+        private const string MY_NAME = "MCF - Easy Onset Package";
+        private string pathDay;
+        private string author;
+        private List<string> listPackage = new List<string>();
+        private StringBuilder myStr = new StringBuilder();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //-- Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //-- No Loading
+        }
+
+        private void UpdateList()
+        {
+            var checkList = checkedListBox1;
+            checkList.Items.Clear();
+
+            if (listPackage.Count == 0) return;
+
+            foreach (var item in listPackage)
+            {
+                checkList.Items.Add(item);
+                checkList.SetItemChecked(checkList.Items.Count - 1, true);
+            }
+        }
+        //-- Ajout des packages à la liste
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var NamePackage = textBox1.Text.Trim();
+            if (NamePackage == string.Empty) return;
+            if (listPackage.Contains(NamePackage))
+            {
+                MessageBox.Show(string.Format("Package : \"{0}\"\nexiste déjà dans la liste", NamePackage), "Ajout impossible");
+                return;
+            }
+            listPackage.Add(NamePackage);
+            UpdateList();
+        }
+
+        //-- Creation des packages
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            author = textBox2.Text.Trim();
+            if (author == string.Empty)
+            {
+                MessageBox.Show("Auteur manquant !");
+                return;
+            }
+            var myDate = DateTime.Today.ToString().Split(new Char[] { ' ' })[0];
+            myDate = myDate.Replace('/', '-');
+            var pathDesk = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var pathEnv = Path.Combine(pathDesk, MY_NAME);
+            pathDay = Path.Combine(pathEnv, string.Format("package_{0}", myDate));
+
+            if (!Directory.Exists(pathEnv))
+                Directory.CreateDirectory(pathEnv);
+
+            if (!Directory.Exists(pathDay))
+                Directory.CreateDirectory(pathDay);
+            
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                var pathPackage = Path.Combine(pathDay, item.ToString());
+                if (!Directory.Exists(pathPackage))
+                    Directory.CreateDirectory(pathPackage);
+
+                CreateFile(item.ToString(), pathPackage);
+            }
+
+            MessageBox.Show("Création avec succès.\nVous trouverez un dossier " +
+                "\n\"" + MY_NAME + "\"\nsur votre bureau.", "Succès");
+        }
+        
+        private string FileDefault(int p_step, string p_fileName)
+        {
+            var myFile = string.Empty;
+            myStr.Clear();
+            switch (p_step)
+            {
+                case 0:
+                    myStr.Append("{\n\t\"author\": \"");
+                    myStr.Append("" + author + "\",");
+                    myStr.AppendLine("\n\t\"version\": \"1.0\",");
+                    myStr.AppendLine("\t\"server_scripts\": [");
+                    myStr.AppendFormat("\t\t\"{0}_s.lua\"\n\t],\n", p_fileName);
+                    myStr.AppendLine("\t\"client_scripts\": [");
+                    myStr.AppendFormat("\t\t\"{0}_c.lua\"", p_fileName);
+                    myStr.AppendLine("\n\t],");
+                    myStr.AppendLine("\t\"files\": []");
+                    myStr.AppendLine("}");
+                    break;
+                case 1:
+                    myStr.Append("function OnPackageStart()");
+                    myStr.AppendFormat("\n\tprint(\"Run - CLIENT - {0}_c.lua\")\nend", p_fileName);
+                    myStr.AppendLine("\nAddEvent(\"OnPackageStart\", OnPackageStart)");
+                    break;
+                case 2:
+                    myStr.Append("function OnPackageStart()");
+                    myStr.AppendFormat("\n\tprint(\"Run - SERVER - {0}_c.lua\")\nend", p_fileName);
+                    myStr.AppendLine("\nAddEvent(\"OnPackageStart\", OnPackageStart)");
+                    break;
+            };
+            return myStr.ToString();
+        }
+
+        private void CreateFile(string p_fileName, string p_pathPackage)
+        {
+            var path = Path.Combine(p_pathPackage, "package.json");
+            File.WriteAllText(path, FileDefault(0, p_fileName));
+            path = Path.Combine(p_pathPackage, string.Format("{0}_c.lua", p_fileName));
+            File.WriteAllText(path, FileDefault(1, p_fileName));
+            path = Path.Combine(p_pathPackage, string.Format("{0}_s.lua", p_fileName));
+            File.WriteAllText(path, FileDefault(2, p_fileName));
+        }
+    }
+}
